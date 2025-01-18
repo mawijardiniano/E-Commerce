b0ead6047c3b532e215732a2d3b0133d78b72959
Mawi <jardinianomarklouisse@gmail.com>
2025-01-18 10:20:23 +0800

    remove secretkeys

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,16 +1,13 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import fs from 'fs';
 
 dotenv.config();
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
-if (!fs.existsSync(serviceAccountPath)) {
-  throw new Error('Service account key file not found');
+if (!serviceAccount) {
+  throw new Error('Service account key not found in environment variables');
 }
-
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
