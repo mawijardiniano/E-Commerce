d3398d6d44a8748c292843cbf8ae9565704feef9
Mawi <jardinianomarklouisse@gmail.com>
2025-01-24 09:59:47 +0800

    implement login function in backend

--- backend/controllers/manualAuthCotroller.js
+++ backend/controllers/manualAuthCotroller.js
@@ -1,6 +1,8 @@
 import firebase from "../firebaseAdmin.js";
 const { db, auth } = firebase;
 import admin from "firebase-admin";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 export const createUser = async (req, res) => {
   const { name, email, password , address, phoneNumber} = req.body;
@@ -31,6 +33,51 @@ export const createUser = async (req, res) => {
     res
       .status(500)
       .json({ message: "Error creating user", error: error.message });
+  }
+};
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Missing required fields: email or password" });
+    }
+
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const userDoc = await db.collection("users").where("email", "==", email).get();
+    if (userDoc.empty) {
+      return res.status(400).json({ message: "User not found in Firestore" });
+    }
+
+    const userData = userDoc.docs[0].data();
+    const isMatch = await bcrypt.compare(password, userData.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign(
+      { email: userData.email, userId: userDoc.docs[0].id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      status: "ok",
+      data: {
+        token,
+        userId: userDoc.docs[0].id,
+        name: userData.name,
+        email: userData.email,
+      },
+      message: "Login Successful",
+    });
+  } catch (error) {
+    console.error("Error during login:", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
 
--- backend/routes/manualAuthController.js
+++ backend/routes/manualAuthController.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createUser, getUsers } from '../controllers/manualAuthCotroller.js';
+import { createUser, getUsers, userLogin } from '../controllers/manualAuthCotroller.js';
 
 const router = express.Router();
 
 
 router.post('/signup', createUser);
+router.post('/login', userLogin)
 router.get('/users', getUsers);
 
 export default router;
