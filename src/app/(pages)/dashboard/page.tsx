0e784855afdfe9b0c5c3fc8d816aa086bf085a7e
Mawi <jardinianomarklouisse@gmail.com>
2025-01-27 14:56:02 +0800

    implement a card component in products

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 import CarouselOffers from "@/components/carousel";
-import Navbar from "@/components/navbar"
+import Navbar from "@/components/navbar";
 import GoogleSignIn from "@/components/googleSignIn";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProductInterface from "@/interfaces/ProductInterface";
 import CategoriesCarousel from "@/components/categoriesCarousel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image1 from "@/assets/you.png";
+import Image from "next/image";
+
 export default function Dashboard() {
-  const [loggedUser, setLoggedUser] = useState("")
+  const [loggedUser, setLoggedUser] = useState("");
   const [product, setProduct] = useState<ProductInterface[]>([]);
 
   const FetchProductAPI = "http://localhost:5000/api/products/fetch-product";
@@ -26,32 +37,48 @@ export default function Dashboard() {
   return (
     <div>
       <header>
-        <Navbar/>
+        <Navbar />
       </header>
       <section className="flex items-center justify-center py-4">
         <CarouselOffers />
       </section>
       <section>
         <div>
-          <p className="text-gray-400 text-lg">Categories</p>
+          <p className="text-gray-400 text-lg font-medium px-8 ">Categories</p>
           <div className="flex py-4">
             <CategoriesCarousel />
           </div>
         </div>
+      </section>
+      <section>
         <div className="bg-gray-300">
           <p>Daily Discovery</p>
+          <div className="flex flex-row px-8 gap-2">
+            {product.length > 0 ? (
+              product.map((products, index) => (
+                <Card key={index} className="w-64 h-64">
+                  <CardHeader>
+                    <CardTitle>
+                      <Image
+                        src={Image1}
+                        alt="Public Folder Image"
+                        width={300}
+                        height={100}
+                        className="rounded-lg"
+                      />
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>{products.name}</CardContent>
+                  <CardFooter>
+                    <p>{products.price}</p>
+                  </CardFooter>
+                </Card>
+              ))
+            ) : (
+              <p>No product available</p>
+            )}
+          </div>
 
-          {product.length > 0 ? (
-            product.map((products, index) => (
-              <div key={index}>
-                <h3>{products.name}</h3>
-                <p>{products.price}</p>
-                <p>{products.category}</p>
-              </div>
-            ))
-          ) : (
-            <p>No product available</p>
-          )}
           <p>Random Items</p>
         </div>
       </section>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -23,9 +23,10 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn("flex flex-col space-y-1.5", className)}
     {...props}
   />
+  
 ))
 CardHeader.displayName = "CardHeader"
 
@@ -57,7 +58,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("p-4 pt-2", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -67,7 +68,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("flex items-center p-4 pt-0", className)}
     {...props}
   />
 ))
